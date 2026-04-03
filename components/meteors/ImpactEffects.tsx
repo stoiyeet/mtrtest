eed5b2a2311a044ba6acc907a68450a9412e2398
stoiyeet <markisaackogan@gmail.com>
2026-04-02 21:44:43 -0400

    Change wording of no moon mortality

--- components/meteors/ImpactEffects.tsx
+++ components/meteors/ImpactEffects.tsx
@@ -414,7 +414,7 @@ export default function ImpactEffects({ effects, mortality, impactLat, impactLon
             </div >
             {celestialBody.name.toLowerCase().includes('moon') ? (
               <div className={styles.sectionInfo}>
-                <span style={{ color: "#d34646" }}>Moon safety note:</span> Don&apos;t bother checking. The number is 0.
+                <span style={{ color: "#d34646" }}>Moon safety note:</span> Nobody has ever died on the moon and no one ever will. True story!
               </div>
             ) : (
               <div className={styles.sectionInfo}>
